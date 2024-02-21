c84b2d08f31914adb30ad8f11d8bea7b6189798c
Daniel <daniel.heuberg@gmail.com>
2024-02-21 08:53:24 +0100

    Add pinned appbar

--- lib/main.dart
+++ lib/main.dart
@@ -38,74 +38,80 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const SelectionArea(
+      body: SelectionArea(
         child: CustomScrollView(
           slivers: [
-            SliverToBoxAdapter(
+            SliverAppBar(
+              expandedHeight: 150.0,
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              flexibleSpace: FlexibleSpaceBar(
+                title: Text(
+                  widget.title,
+                ),
+              ),
+              pinned: true,
+            ),
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                 data: '### Header',
               ),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                 data: '### Header',
               ),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                 data: '### Header',
               ),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
             ),
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: SimpleclubMarkdownComponent(
                   data:
                       'Lorem ipsum dolor sit amet, Foo consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
